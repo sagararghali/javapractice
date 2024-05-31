86b2d124467daadd9d270c88f6218d98621b1b2d
sagararghali <sagararghalimagar@gmail.com>
2024-05-31 12:31:05 +0545

    navbar

--- pattern.js
+++ pattern.js
@@ -14,3 +14,86 @@ let sap =()=>{
    }
    let a=sap()
    console.log(a)
+
+   //             "use strict";  
+        //             //to insert variable  
+        // // let a=34;
+        // //         console.log(a);
+        // // function sap(){  //sap()
+        
+        
+        
+        // //     console.log("hey sagar");
+        // // }
+        
+        
+        // //function inside the object
+        // // let obj1={
+        // //     r:1,
+        // //     b:3,
+        // //     func:function (number){  //obj1.func(50)
+        // //         console.log("the number is:"+number);
+        // //     }
+        // // }
+        
+        // //conformation window will be popup 
+        // // document.addEventListener("click", function click(){
+        // //     console.log("clicked");
+        // //     //alert("hello");
+        // //     let c=confirm("are you sure");
+        // //     console.log(c);
+        // // })
+        // // undefined variables inside the variable
+        // // function fun(number){
+        // //     console.log("the value is :"+this);
+        // // }
+        // // fun();
+        
+        // //arrow function in java with undefined variable"this"
+        // // let obj2={
+           
+        // //  newfun: (number)=> {
+        // //     console.log("the value is:"+this);
+        // // }}
+        // // obj2.newfun();
+        // //call back function
+        // // setTimeout(() => {
+        // //     console.log("i have come ");
+        // // },3000);
+        // // console.log("hii i have come first");
+        
+        // // console.log(x);
+        // // var x=10;
+        // // console.log(x)
+        // // lol()
+        
+        // //  function lol(){
+        // //     console.log("sagar")
+        // // }
+        
+        
+        // let sum=()=>{
+        //     return 5;
+        
+        // }
+        // let a =sum();
+        // console.log(a);
+        //  let add=()=>{
+        //      const x={
+        // name:"sagar",
+        // exp:"5"
+        //     }
+        //     console.log(x.name)
+        // }
+        
+        
+        // function sagar(){
+        //     console.log("my name is sagar")
+        // }
+        // sagar()
+        const toggleButton = document.getElementById('navbar-toggle');
+const navbarLinks = document.getElementById('navbar-links');
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+});
