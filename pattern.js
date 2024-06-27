0c8c280c8a5d10d8eef5faa38598f58e22055e68
sagararghali <sagararghalimagar@gmail.com>
2024-06-27 16:24:55 +0545

    update

--- pattern.js
+++ pattern.js
@@ -21,22 +21,39 @@
 // }
  
 //map method
-let a =[10,20,30,40,50]
-a.map((value,index,array)=>{
-console.log(value,index,array);
-})
+// let a =[10,20,30,40,50]
+// a.map((value,index,array)=>{
+// console.log(value,index,array);
+// })
 
-// filter method
-let b=[11,22,33,44,55]
-let c=b.filter((element)=>{
-return element<30;
-})
-console.log(c);
+// // filter method
+// let b=[11,22,33,44,55]
+// let c=b.filter((element)=>{
+// return element<30;
+// })
+// console.log(c);
 
-// reduce method
-let d=[12,23,34,45,56]
-let e=d.reduce((e1,e2)=>{
-return e1+e2;
-})
-console.log(e);
+// // reduce method
+// let d=[12,23,34,45,56]
+// let e=d.reduce((e1,e2)=>{
+// return e1+e2;
+// })
+// console.log(e);
+
+//create an array of numbers and take input from the user to add numbers to the array
+// let x=prompt("enter element of an array");//this was my logic
+// console.log(x); 
+// let sap=Array.from(x)
+// console.log(sap);
+//  let y=sap.push(10,20,30);
+//  console.log(sap);
+
+//keep adding numbers to the array if user enters 0 terminate the program
+// let b=prompt("enter elements");
+// console.log(b);
+// do{
+// let c=Array.from(b)
+// let a=[1,2,3]
+// a.push(c)
+// }while(b!=0);
 
