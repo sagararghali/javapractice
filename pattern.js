d2d7a988a24e17e754118579379944a2957b3603
sagararghali <sagararghalimagar@gmail.com>
2024-06-27 12:16:16 +0545

    update

--- pattern.js
+++ pattern.js
@@ -1,7 +1,24 @@
 //loop
-// for (let i=2; i<=100; i++){
-//     if (i%2 ==0){
-//         console.log(i);
-//     }
-// }
+// for each
+let num =[1,2,3,4,5,6]
+num.forEach(function(num){
+console.log(num);
+})
+
+//array from
+let arr="sagar";
+let x=Array.from(arr);
+console.log(x);
+
+//for of
+for(let i of arr){
+  console.log(i);
+}
+
+//for in
+for(let i in arr){
+  console.log(i); //it display key/address if element
+}
+ 
+//map
 
